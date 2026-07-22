edb3a2e13d9d4e51eb4a48267cacc9df441c6ba9
Kim JunYeop <108616598+yeopbuddy@users.noreply.github.com>
2026-07-23 00:05:22 +0900

    [level 3] Title: 정수 삼각형, Time: 0.28 ms, Memory: 8.54 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    
+    int answer = 0;
+    
+    for(int i = 1; i < triangle.size(); i++){
+        for(int j = 0; j < i + 1; j++){
+            if(j == 0){
+                triangle[i][j] += triangle[i - 1][j];
+            }else if(j == i){
+                triangle[i][j] += triangle[i - 1][j - 1];
+            }else{
+                triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+            }
+        }
+    }
+    
+    for(int i = 0; i < triangle.size(); i++){
+        answer = max(answer, triangle[triangle.size() - 1][i]);
+    }
+    
+    return answer;
+}
